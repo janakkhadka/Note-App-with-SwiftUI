43909fcaf4e3f2b9a62fc69dccf676ceeb34b60d
Janak Khadka <janakkhadka08@gmail.com>
2025-02-15 23:28:13 +0545

    delete notes, with this all desire product is completed for now

--- My Note/My Note/ContentView.swift
+++ My Note/My Note/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
     @State private var isShowingSheet = false
     @State private var selectedNote: Note = Note(title: "", description: "")
     
-    @Query(sort: \Note.title) var swiftDataNotes: [Note]
+    @Query(sort: \Note.createdAt) private var swiftDataNotes: [Note] // yo chai database maa all data retrieve garera according to date sort garna lai ho
+    @Environment(\.modelContext) private var context
     
     var body: some View {
         GeometryReader { geometry in  //GeometryReader le chai parent view vannale yaa chai Content view ko size and position reads garera adjust garna help garxa
@@ -54,6 +55,14 @@ struct ContentView: View {
                                 print("note tapped")
                                 selectedNote = note
                                 isShowingSheet = true
+                            }
+                            .contextMenu {
+                                Button(action: {
+                                    context.delete(note)
+                                }){
+                                    Text("Delete")
+                                    Image(systemName: "trash")
+                                }
                             }
                     }
                 }

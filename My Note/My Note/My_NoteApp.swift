a420bce03da80799dc2f8f12ea1e4f6e5d91d226
Janak Khadka <janakkhadka08@gmail.com>
2025-02-15 11:41:38 +0545

    project created for notes app

---
+++ My Note/My Note/My_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_NoteApp.swift
+//  My Note
+//
+//  Created by Janak Khadka on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct My_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My Note/My NoteTests/My_NoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_NoteTests.swift
+//  My NoteTests
+//
+//  Created by Janak Khadka on 15/02/2025.
+//
+
+import Testing
+@testable import My_Note
+
+struct My_NoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
